fb9c0ca66b2537ba42091c13792044aacaaf329d
AgustinSotero <52137971+AgustinSF@users.noreply.github.com>
2023-02-05 12:42:29 +0000

    added logical_or frontend test

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
@@ -506,6 +506,33 @@ def test_jax_numpy_bitwise_xor(
     )
 
 
+@handle_frontend_test(
+    fn_tree="jax.numpy.logical_or",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("bool"), num_arrays=2
+    ),
+    test_with_out=st.just(False),
+)
+def test_jax_numpy_logical_or(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x1=x[0],
+        x2=x[1],
+    )
+
+
 # any
 @handle_frontend_test(
     fn_tree="jax.numpy.any",
